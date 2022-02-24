953d9dd3508de98b42eb3e846c990f902cfdc7f2
severHatice <95497761+severHatice@users.noreply.github.com>
2022-02-24 23:02:11 +0100

    dto package

--- src/main/java/com/dev/carrental_project/dto/UserDTO.java
+++ src/main/java/com/dev/carrental_project/dto/UserDTO.java
@@ -17,10 +17,26 @@ import java.util.Set;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
+    //kullanicinin id ve pass gibi bilgilerini gormesini istemiyoruz.
+    //id ve pass olmadan tum bilgilerini iceren class olusturup bunun uzerine calisiyoruz
+    //DTO=data tranfert object
 
+
+    public UserDTO(String firstName, String lastName, String phoneNumber,//pass ve id siz const
+                   String email, String address, String zipCode, Boolean builtIn, Set<String> roles) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.builtIn = builtIn;
+        this.roles = roles;
+    }
+    //class entity table olusturmadigi icin column kullanilmaz
     @Size(max = 15)
     @NotNull(message = "Please enter your first name")
     private String firstName;
@@ -37,48 +53,28 @@ public class UserDTO {
     @Size(min = 14, max = 14)
     @NotNull(message = "Please enter your phone number")
     private String phoneNumber;
-
     @Email(message = "Please enter valid email")
     @Size(min = 5, max = 150)
     @NotNull(message = "Please enter your email")
     private String email;
-
     @Size(max = 250)
     @NotNull(message = "Please enter your address")
     private String address;
-
     @Size(max = 15)
     @NotNull(message = "Please enter your zip code")
     private String zipCode;
-
-
-    private Boolean builtIn ;
-
+    private Boolean builtIn;//admin olarak bilgi eklendiginde, silinmesini istemiyorsa ileriki zamanlarda true diyoruz
     private Set<String> roles;
 
-    public UserDTO(String firstName, String lastName, String phoneNumber, String email,
-                   String address, String zipCode, Boolean builtIn, Set<String> roles) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.address = address;
-        this.zipCode = zipCode;
-        this.builtIn = builtIn;
-        this.roles = roles;
-    }
-
-    public void setRoles(Set<Role>roles){
-        Set<String>roles1=new HashSet<>();
-
-        Role[] role=roles.toArray(new Role[roles.size()]);
-
-        for (int i=0;i<roles.size();i++){
-            if(role[i].getName().equals(UserRole.ROLE_ADMIN))
-                roles1.add("Administirator");
+    public void setRoles(Set<Role> roles) {//???????????????
+        Set<String> roles1 = new HashSet<>();
+        Role[] role = roles.toArray(new Role[roles.size()]);
+        for (int i = 0; i < roles.size(); i++){
+            if (role[i].getName().equals(UserRole.ROLE_ADMIN))
+                roles1.add("Administrator");
             else
                 roles1.add("Customer");
         }
-        this.roles=roles1;
+        this.roles = roles1;
     }
 }
